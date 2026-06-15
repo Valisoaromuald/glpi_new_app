5a61f7e9fe08205f208e7e4568196767d2046321
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-06-15 08:14:57 +0300

    fix:correction de nom de colonne dans la fonction d'importation numero 1

--- src/services/import/importService.ts
+++ src/services/import/importService.ts
@@ -218,7 +218,7 @@ export default class ImportService {
                                     let endpointModelName: string = `${row["Item_Type"]}Model`;
                                     let inArray = this.isInArray(MODEL_NAMES_LIST, endpointModelName);
                                     if (inArray) {
-                                        let mdl: Partial<AssetModel> = { name:  row["Item_Type"]}
+                                        let mdl: Partial<AssetModel> = { name: row["Model"] }
                                         let object: Object = AssetService.createModelObject(mdl, endpointModelName)
                                         assetModel = await glpiApi.postV1(endpointModelName, object)
                                         assetModel.id = assetModel.data.id
@@ -238,7 +238,7 @@ export default class ImportService {
                                     let endpointTypeName: string = `${row["Item_Type"]}Type`;
                                     let inArray = this.isInArray(TYPE_NAMES_LIST, endpointTypeName);
                                     if (inArray) {
-                                        let type: Partial<AssetType> = { name: row["Item_Type"]}
+                                        let type: Partial<AssetType> = { name: row["Item_Type"] }
                                         let object: Object = AssetService.createTypeObject(type)
                                         assetType = await glpiApi.postV1(endpointTypeName, object)
                                         assetType.id = assetType.data.id
@@ -391,7 +391,7 @@ export default class ImportService {
         }
 
         // Si le ticket est clos, GLPI exige aussi les dates de résolution/clôture
-        if (row["Status"]?.trim(),"Closed") {
+        if (this.equalsIgnoreCase(row["Status"]?.trim(), "Closed")) {
             const closeDate = this.formatDate(row)  // même logique que pour 'date'
 
             object = {
@@ -533,12 +533,10 @@ export default class ImportService {
         if (!strValue) {
             return result;
         }
-        if (strValue.includes(".") || strValue.includes(",")) {
-            if (strValue.includes(",")) {
-                strValue = replaceChar(strValue, ',', '.')
-            }
-            result = this.zeroIfNegative(parseFloat(strValue))
+        if (strValue.includes(",")) {
+            strValue = replaceChar(strValue, ',', '.');
         }
+        result = this.zeroIfNegative(parseFloat(strValue));
         return result;
     }
     async importTicketCosts(csv: CsvResult, onProgress?: (resource: string, done: number, total: number) => void): Promise<string> {
