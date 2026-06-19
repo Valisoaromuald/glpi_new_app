b12291dea2c1304f989d68af88c28483bc8ffb1d
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-06-20 00:52:16 +0300

    feat:types pour les couts de ticket

---
+++ shared-types/ticketCost.types.ts
@@ -0,0 +1,9 @@
+export interface ITicketCost{
+    id:number
+    cost:number
+    cost_type: string
+    category: string
+    creation_date: string
+    item_id : number
+    ticket_id:number
+}
---
+++ shared-types/ticketCostGrouped.types.ts
@@ -0,0 +1,8 @@
+export interface ITicketCostGrouped {
+    category: string;
+    super_cost: number;
+    glpi_cost: number;
+    reopening: number;
+    close: number;
+    total: number;
+}
