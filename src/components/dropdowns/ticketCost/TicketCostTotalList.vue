dc7fdc55c1a33d870033604dff4780fbc5f809f9
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-06-20 22:18:40 +0300

    fix:utilisation du composable useTicketCost

--- src/components/dropdowns/ticketCost/TicketCostTotalList.vue
+++ src/components/dropdowns/ticketCost/TicketCostTotalList.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { onMounted } from 'vue'
 import NewAppApi from '@/api/newAppApi'
 import type { ITicketCostGrouped } from 'shared-types';
-// import util
+import { useRouter } from 'vue-router';
+import { useTicketCost } from '@/composables/useTicketCost'
 
-
+const router = useRouter()
 const props = defineProps<{ ticketId: number }>()
-
 const newAppApi = new NewAppApi()
-const categories = ref<string[]>([])
-const selectedCategory = ref<string>('')
-const rows = ref<ITicketCostGrouped[]>([])
-const isLoading = ref<boolean>(false)
-const error = ref<string | null>(null)
-const glpiCostTotal = ref<number>(0)
-const superCostTotal = ref<number>(0)
-const reopeningCostTotal = ref<number>(0)
-const globalTotal = ref<number>(0)
-
+const { rows, isLoading, error, glpiCostTotal, superCostTotal, reopeningCostTotal, globalTotal, computeTotals } = useTicketCost()
 
 async function loadGroupedCosts() {
     isLoading.value = true
@@ -26,68 +17,74 @@ async function loadGroupedCosts() {
         const { data } = (await newAppApi.get<{ data: ITicketCostGrouped[] }>(
             `tickets/${props.ticketId}/costs/grouped`)).data
         rows.value = data
-        rows.value.forEach(element => {
-            glpiCostTotal.value += element.glpi_cost
-            superCostTotal.value += element.super_cost
-            reopeningCostTotal.value += element.reopening
-            globalTotal.value += element.total
-        });
+        computeTotals()
     } catch (err) {
         error.value = 'Impossible de charger les coûts'
     } finally {
         isLoading.value = false
     }
 }
-watch(selectedCategory, loadGroupedCosts)
 onMounted(loadGroupedCosts)
 </script>
 
 <template>
     <div class="bg-white border border-gray-200 rounded-xl overflow-hidden si">
-        <div class="px-4 py-3 border-b border-gray-100 flex items-center justify-between">
-            <span class="text-sm font-medium text-black">
-                <div class="flex flex-row gap-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-4">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    <span>Couts des tickets</span>
-                </div>
-            </span>
+        <div class="overflow-x-auto">
+            <table class="w-full text-sm border-collapse">
+                <thead>
+                    <tr class="bg-blue-800  text-left text-white font-bold">
+                        <th class="px-3 py-2 font-medium">Catégorie d'Actifs</th>
+                        <th class="px-3 py-2 font-medium">Super cost</th>
+                        <th class="px-3 py-2 font-medium">Glpi cost</th>
+                        <th class="px-3 py-2 font-medium">Réouverture</th>
+                        <th class="px-3 py-2 font-medium">Total</th>
+                        <th class="px-3 py-2 font-medium">Action(s)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-if="error">
+                        <td colspan="6" class="text-sm text-center text-red-500 py-2">
+                            {{ error }}
+                        </td>
+                    </tr>
+                    <tr v-else-if="isLoading">
+                        <td colspan="6" class="text-sm text-gray-500">Chargement...</td>
+                    </tr>
+                    <template v-else>
+                        <tr v-for="row in rows" class="border-b border-gray-100">
+                            <td class="px-3 py-2">{{ row.category }}</td>
+                            <td class="px-3 py-2">{{ row.super_cost }}</td>
+                            <td class="px-3 py-2">{{ row.glpi_cost }}</td>
+                            <td class="px-3 py-2">{{ row.reopening }}</td>
+                            <td class="px-3 py-2 font-semibold">{{ row.total }}</td>
+                            <td class="px-3 py-2 font-semibold">
+                                <button
+                                    class="flex items-center gap-1 bg-green-400 hover:bg-green-500 text-white text-sm font-semibold px-3 py-1.5 rounded-lg transition-colors cursor-pointer"
+                                    @click="router.push(`/detail/${row.category}`)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.641 0-8.574-3.007-9.964-7.178Z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+                                    Détails
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-3 py-2 font-bold">Total</td>
+                            <td class="px-3 py-2">{{ superCostTotal }}</td>
+                            <td class="px-3 py-2">{{ glpiCostTotal }}</td>
+                            <td class="px-3 py-2">{{ reopeningCostTotal }}</td>
+                            <td class="px-3 py-2 font-semibold">{{ globalTotal }}</td>
+                        </tr>
+                    </template>
+                    <tr v-if="rows.length === 0 && !error">
+                        <td colspan="7" class="px-3 py-4 text-center text-gray-400">Aucun coût n'a ete enregistre</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-        <p v-if="error" class="text-sm text-red-500">{{ error }}</p>
-        <p v-else-if="isLoading" class="text-sm text-gray-500">Chargement...</p>
-
-        <table v-else class="w-full text-sm border-collapse">
-            <thead>
-                <tr class="bg-gray-100 text-left text-black font-bold">
-                    <th class="px-3 py-2 font-medium">Catégorie</th>
-                    <th class="px-3 py-2 font-medium">Super cost</th>
-                    <th class="px-3 py-2 font-medium">Glpi cost</th>
-                    <th class="px-3 py-2 font-medium">Réouverture</th>
-                    <th class="px-3 py-2 font-medium">Total</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="row in rows"  class="border-b border-gray-100">
-                    <td class="px-3 py-2">{{ row.category }}</td>
-                    <td class="px-3 py-2">{{ row.super_cost }}</td>
-                    <td class="px-3 py-2">{{ row.glpi_cost }}</td>
-                    <td class="px-3 py-2">{{ row.reopening }}</td>
-                    <td class="px-3 py-2 font-semibold">{{ row.total }}</td>
-                </tr>
-                <tr>
-                    <td class="px-3 py-2 font-bold">Total</td>
-                    <td class="px-3 py-2">{{  superCostTotal }}</td>
-                    <td class="px-3 py-2">{{ glpiCostTotal }}</td>
-                    <td class="px-3 py-2">{{ reopeningCostTotal }}</td>
-                    <td class="px-3 py-2 font-semibold">{{globalTotal}}</td>
-                </tr>
-                <tr v-if="rows.length === 0">
-                    <td colspan="7" class="px-3 py-4 text-center text-gray-400">Aucun coût n'a ete enregistre</td>
-                </tr>
-            </tbody>
-        </table>
     </div>
 </template>
