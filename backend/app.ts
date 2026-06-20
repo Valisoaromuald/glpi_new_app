582e61f80b3d254ffe66bbf6622ca74aca58272f
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-06-20 22:40:34 +0300

    chore: fichier d'exposition de routes du dossier backend

---
+++ backend/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import ticketRoutes from "./modules/tickets/ticket.routes"
+import { errorHandler } from './middleware/error.middleware';
+import kanbanRoutes from './modules/kanban/kanban.routes';
+import resetRoutes from './modules/reset/reset.routes';
+import ticketCostRoutes from './modules/ticketCost/ticketCost.routes';
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/tickets', ticketRoutes);
+app.use('/api/ticketCosts', ticketCostRoutes);
+app.use('/api/kanban-config', kanbanRoutes);
+
+app.use('/api/reset', resetRoutes);
+app.use(errorHandler);
+
+export default app; 
