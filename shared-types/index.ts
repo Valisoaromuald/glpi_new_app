cf5e698bb4261a44c1de579033f4990d0d83e88f
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-06-20 22:33:26 +0300

    chore:creation de fichier indx pour export des types partages

---
+++ shared-types/index.ts
@@ -0,0 +1,5 @@
+export * from './ticket.types';
+export * from './kanban.types';
+export  * from'./ticketCost.types';
+export  * from'./ticketItem.types';
+export  * from'./ticketCostGrouped.types';
