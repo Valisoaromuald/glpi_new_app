4318a5cb370f111cb1e8128985e3f68ee45a1801
Valisoaromuald <romualdrabenandrasana@gmail.com>
2026-06-20 22:30:54 +0300

    fix:ajout d'une propriete qui permet d'ajouter n'importe quelle cle dans l'interface

--- shared-types/ticketCostGrouped.types.ts
+++ shared-types/ticketCostGrouped.types.ts
@@ -1,8 +1,10 @@
 export interface ITicketCostGrouped {
     category: string;
+    item_id:number,
     super_cost: number;
     glpi_cost: number;
     reopening: number;
     close: number;
     total: number;
+    [key:string]: any
 }
